9c0637586b77a7d5161364c9d4b9b244043e9064
DablioDC <wellingtondiegocardoso@gmail.com>
2024-11-10 10:21:17 -0300

    feat: first commit

---
+++ src/main/java/br/com/dabliodc/compass_uol/CompassUolApplication.java
@@ -0,0 +1,13 @@
+package br.com.dabliodc.compass_uol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassUolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompassUolApplication.class, args);
+	}
+
+}
