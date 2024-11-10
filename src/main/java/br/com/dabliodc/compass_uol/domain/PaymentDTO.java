40edcd3dca7273b71f306c06181ab6c226e8e2eb
DablioDC <wellingtondiegocardoso@gmail.com>
2024-11-10 11:35:24 -0300

    feat: Removed field vendor from PaymentDTO

--- src/main/java/br/com/dabliodc/compass_uol/domain/PaymentDTO.java
+++ src/main/java/br/com/dabliodc/compass_uol/domain/PaymentDTO.java
@@ -1,6 +1,5 @@
 package br.com.dabliodc.compass_uol.domain;
 
-import br.com.dabliodc.compass_uol.model.Vendor;
 import br.com.dabliodc.compass_uol.utils.PaymentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,7 +15,6 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class PaymentDTO implements Serializable {
     private String id;
-    private Vendor vendor;
     private LocalDate payDay;
     private BigDecimal amountPaid;
     private PaymentStatus paymentStatus;
